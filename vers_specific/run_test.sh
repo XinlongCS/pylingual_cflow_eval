3bd3070058e35d033dc5326fca7361fa577ac3ba
Xinlong Hu <xinlonghucs@gmail.com>
2025-08-13 17:58:32 -0500

    fast eval

--- vers_specific/run_test.sh
+++ vers_specific/run_test.sh
@@ -2,12 +2,8 @@
 rm -rf /tmp/cflow_test/3*
 
 tmux new-session -d -s py_versions
-for version in 6 7 8 9 10 11 12 13; do
-    tmux split-window -t py_versions "python ~/pylingual/dev_scripts/cflow.py ~/pylingual_eval/py_files/total/py3_${version}_files.txt -v 3.${version}"
+for version in 13 12 11 10 9 8 7 6; do
+    tmux split-window -t py_versions "python ~/pylingual/dev_scripts/cflow.py ~/pylingual_eval/py_files/med_eval/med_eval_3_${version}.txt -v 3.${version}"
     tmux select-layout -t py_versions tiled
 done
 tmux attach -t py_versions
-
-for version in 6 7 8 9 10 11 12 13; do
-    grep -o \"/syssec-nas0/pyc-research/compiled_evaluation_targets/usenix_test_set/python-3.${version}/[^"]*\" /tmp/cflow_test/3.${version}/py3_${version}_files_0/results.json | sort -u > med_eval_3_${version}.txt
-done
