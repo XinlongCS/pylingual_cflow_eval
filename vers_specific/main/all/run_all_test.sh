7559570f1883f182013e4f6a49ddde423278e825
Xinlong Hu <xinlonghucs@gmail.com>
2025-08-14 10:16:44 -0500

    nit + ran all test

---
+++ vers_specific/main/all/run_all_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -rf /tmp/cflow_test/3*
+
+tmux new-session -d -s py_versions
+for version in 13 12 11 10 9 8 7 6; do
+    tmux split-window -t py_versions "python ~/pylingual/dev_scripts/cflow.py ~/pylingual_eval/py_files/total/py3_${version}_files.txt -v 3.${version}"
+    tmux select-layout -t py_versions tiled
+done
+tmux attach -t py_versions
